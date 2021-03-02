0d3bdefdafd3c8eef7ca982eb0b2d59c9558986a
jjinlee92 <36918332+jjinlee92@users.noreply.github.com>
2021-03-02 01:36:45 -0800

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,11 @@
+<?php 
+$to = "slee165@oinside.artcenter.edu"; // this is your Email address
+$from = $_POST['email']; //
+$name = $_POST['name'];
+$consent = "consent";
+$subject = "Copy of your form submission";
+
+mail($to,$name,$consent,$subject);
+mail($from,$consent,$subject); // sends a copy of the message to the sender
+echo "Mail Sent. Thank you. I will contact you shortly.";
+?>
