5d8381d1f2ab63f636c3af4528dd2d8e32508654
jjinlee92 <36918332+jjinlee92@users.noreply.github.com>
2021-03-02 01:54:21 -0800

    Add files via upload

--- submit.php
+++ submit.php
@@ -1,11 +1,26 @@
-<?php 
-$to = "slee165@oinside.artcenter.edu"; // this is your Email address
-$from = $_POST['email']; //
-$name = $_POST['name'];
-$consent = "consent";
-$subject = "Copy of your form submission";
-
-mail($to,$name,$consent,$subject);
-mail($from,$consent,$subject); // sends a copy of the message to the sender
-echo "Mail Sent. Thank you. I will contact you shortly.";
+<!-- <?php 
+    if(isset($_POST["submit"])) {
+    $to = "slee165@oinside.artcenter.edu"; // this is your Email address
+    $from = $_POST['email']; //
+    $name = $_POST['name'];
+    $consent = "consent";
+    $subject = "Copy of your form submission";
+            
+    mail($to,$name,$consent,$subject);
+    mail($from,$consent,$subject); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you. I will contact you shortly.";
+}
+?> -->
+<?php
+if ( !empty( $_POST ) ){
+    $to = "slee165@oinside.artcenter.edu"; // this is your Email address
+    $from = $_POST['email']; //
+    $name = $_POST['name'];
+    $consent = "consent";
+    $subject = "Copy of your form submission";
+            
+    mail($to,$name,$consent,$subject);
+    mail($from,$consent,$subject); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you. I will contact you shortly.";
+}
 ?>
